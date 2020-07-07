a87aa83fb9dd6056bc79a11704f6c3a633d3519e
L-SssA <1091411975@qq.com>
2020-07-07 22:24:03 +0800

    feat:修改了导航栏的下拉菜单的路由

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,16 +8,15 @@ module.exports = {
     themeConfig: {
         nav: [
             { text: '主页', link: '/' },
-            { text: '文章', link: '/Vuepress/Vuepress的快速构建与部署' },
             {
                 text: '列表',
                 items: [{
-                        text: '主页',
-                        link: '/'
+                        text: 'VuePress',
+                        link: '/VuePress/'
                     },
                     {
-                        text: '文章',
-                        link: '/VuePress/Vuepress的快速构建与部署'
+                        text: 'CSS',
+                        link: '/CSS/'
                     },
                 ]
             }
@@ -26,14 +25,22 @@ module.exports = {
         lastUpdated: '更新时间',
         smoothScroll: true
     },
-    plugins: [
-        [
-            '@vuepress/last-updated',
-            {
-                transformer: (timestamp) => {
-                    return moment(timestamp).format('lll');
-                }
+    plugins: {
+        '@vssue/vuepress-plugin-vssue': {
+            // 设置 `platform` 而不是 `api`
+            platform: 'github-v4',
+
+            // 其他的 Vssue 配置
+            owner: 'L-SssA',
+            repo: 'Blog',
+            clientId: '35536f68948683f7b48d',
+            clientSecret: '9661f0604ded7df0382c49c76fd242b129a8f1ab',
+            autoCreateIssue: true,
+        },
+        '@vuepress/last-updated': {
+            transformer: (timestamp) => {
+                return moment(timestamp).format('lll');
             }
-        ]
-    ]
+        }
+    }
 }
